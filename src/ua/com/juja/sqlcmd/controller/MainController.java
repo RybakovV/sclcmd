a6ab21c8b88ab8b45f84d584e2d133fe0e361d72
MEBELBOS-2 <rwmail3@gmail.com>
2016-09-03 12:44:50 +0300

    Є комнади: list - друк таблиць підключеної бази даних help - друк списуку комнад connect - підключеення до іншої бази даних, або перепідключення до цієї ж exit - вихід з програми

--- src/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/ua/com/juja/sqlcmd/controller/MainController.java
@@ -6,8 +6,10 @@ import ua.com.juja.sqlcmd.model.PostgresqlDatabaseManager;
 import ua.com.juja.sqlcmd.viuw.Console;
 import ua.com.juja.sqlcmd.viuw.View;
 
+import java.util.Arrays;
 
-public class MainController {
+
+class MainController {
 
 
     private View view;
@@ -18,15 +20,48 @@ public class MainController {
         this.manager = manager;
     }
 
-    public static void main(String[] args) {
-        View view = new Console();
-        DatabaseManager manager = new PostgresqlDatabaseManager();
-    }
-
-
     void run(){
         connectToDb();
 
+        label:
+        while (true) {
+            view.write("Enter command (or command 'help' for help): ");
+            String command = view.read();
+            switch (command) {
+                case "list":
+                    doList();
+                    break;
+                case "help":
+                    doHelp();
+                    break;
+                case "connect":
+                    connectToDb();
+                    break;
+                case "exit":
+                    break label;
+                default:
+                    view.write("non-existent command: " + command);
+                    break;
+            }
+
+        }
+    }
+
+    private void doList(){
+        String[] tables = manager.getAllTablesOfDataBase();
+        view.write(Arrays.toString(tables));
+    }
+
+    private void doHelp(){
+        view.write("Possible commands:");
+        view.write("\tlist");
+        view.write("\t\tprint all tables of the connected database");
+        view.write("\texit");
+        view.write("\t\tto exit from the program");
+        view.write("\tconnect");
+        view.write("\t\tto connect to another database");
+        view.write("\thelp");
+        view.write("\t\tprint possible commands");
 
     }
 
@@ -35,16 +70,10 @@ public class MainController {
         while (true){
             view.write("Enter Database name: ");
             String databaseName = view.read();
-            //manager.setDatabaseName(databaseName);
-
             view.write("Enter userName");
             String userName = view.read();
-            //manager.setUserName(userName);
-
             view.write("Enter password");
             String userPassword = view.read();
-            //manager.setUserPassword(userPassword);
-
             try{
                 manager.connectToDataBase(databaseName, userName, userPassword);
                 if (manager.getVersionDatabase().equals("MySQL")){
